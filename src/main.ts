b35552dc360ea0e9e356b7535cfc868820175e2a
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-05-18 19:29:35 -0300

    configured fontawesome

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/style/style.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 Vue.config.productionTip = false;
 
