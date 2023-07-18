da57fdc38d93e569a8333e14e4a7e9193c61dd45
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-07-18 19:11:18 -0300

    created other main topics for the categories

--- src/models/Categories.ts
+++ src/models/Categories.ts
@@ -9,4 +9,34 @@ export const Categories: CategoriesList[] = [
 	{
 		...clothingAndShoesCategories,
 	},
+	{
+		text: "Home & Living",
+		link: "#",
+		type: "link",
+	},
+	{
+		text: "Wedding & Party",
+		link: "#",
+		type: "link",
+	},
+	{
+		text: "Toys & Entertainment",
+		link: "#",
+		type: "link",
+	},
+	{
+		text: "Art & Collectibles",
+		link: "#",
+		type: "link",
+	},
+	{
+		text: "Craft Supplies & Tools",
+		link: "#",
+		type: "link",
+	},
+	{
+		text: "Vintage",
+		link: "#",
+		type: "link",
+	},
 ];
