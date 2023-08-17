b97c3958ffa6d0a20ec1b8f27c5b64b864a4a0d0
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-08-17 15:21:43 -0300

    coded basic welcome text

---
+++ src/components/RecentActivity.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="w-full bg-toastedYellow text-center text-4xl">
+		<div class="py-5">Welcome to Etsy, <u>Carolina!</u></div>
+	</div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
 			backgroundColor: {
 				lightGray: "#e1e3df",
 				lighterGray: "#F3F3F3",
+				toastedYellow: "#FDEBD2",
 			},
 			letterSpacing: {
 				regular: "1px",
