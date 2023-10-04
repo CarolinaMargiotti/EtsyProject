df43ddcbeb2148122dee9d91c5de29530c1f58b0
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-10-04 13:23:40 -0300

    made responsive changes to footer

--- src/components/Footer/BeigeFooterContent.vue
+++ src/components/Footer/BeigeFooterContent.vue
@@ -2,13 +2,8 @@
 	<div class="w-full flex flex-col items-center mb-12">
 		<h2 class="text-4xl">What is Etsy?</h2>
 		<a href="#" class="underline"> Read our wonderfully weird story </a>
-		<div class="grid grid-cols-3 mt-10 w-8/12">
-			<h3 class="font-bold text-2xl pb-5">A community doing good</h3>
-			<h3 class="font-bold text-2xl pb-5">
-				Support independent creators
-			</h3>
-			<h3 class="font-bold text-2xl pb-5">Peace of mind</h3>
-			<div class="leading-6">
+		<div class="grid gap-4 grid-cols-1 lg:grid-cols-3 mt-10 w-8/12">
+			<ContentTitleSection title="A community doing good" class="mb-7">
 				Etsy is a global online marketplace, where people come together
 				to make, sell, buy, and collect unique items. We’re also a
 				community pushing for positive change for small businesses,
@@ -17,17 +12,20 @@
 					Here are some of the ways we’re making a positive impact,
 					together.
 				</div>
-			</div>
-			<div class="leading-6">
+			</ContentTitleSection>
+			<ContentTitleSection
+				title="Support independent creators"
+				class="mb-7"
+			>
 				There’s no Etsy warehouse – just millions of people selling the
 				things they love. We make the whole process easy, helping you
 				connect directly with makers to find something extraordinary.
-			</div>
-			<div class="leading-6">
+			</ContentTitleSection>
+			<ContentTitleSection title="Peace of mind">
 				Your privacy is the highest priority of our dedicated team. And
 				if you ever need assistance, we are always ready to step in for
 				support.
-			</div>
+			</ContentTitleSection>
 		</div>
 		<div class="mt-12 text-center">
 			<h4 class="font-bold text-xl mb-4">
@@ -40,9 +38,10 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 import OutlinedButton from "../OutlinedButton.vue";
+import ContentTitleSection from "./ContentTitleSection.vue";
 
 @Component({
-	components: { OutlinedButton },
+	components: { OutlinedButton, ContentTitleSection },
 })
 export default class BeigeFooterContent extends Vue {}
 </script>
---
+++ src/components/Footer/ContentTitleSection.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="leading-6 text-center lg:text-start">
+		<h3 class="font-bold text-2xl pb-5">{{ title }}y</h3>
+		<slot></slot>
+	</div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class ContentTitleSection extends Vue {
+	@Prop()
+	public title!: string;
+}
+</script>
