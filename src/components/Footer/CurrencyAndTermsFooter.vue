6580ffb6a16945a470908e1463f032b6cefefe2e
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-10-06 13:53:41 -0300

    changed grid to flex from terms component

--- src/components/Footer/CurrencyAndTermsFooter.vue
+++ src/components/Footer/CurrencyAndTermsFooter.vue
@@ -2,13 +2,13 @@
 	<div class="flex justify-center bg-bluePurple py-5 text-white">
 		<div class="flex justify-between w-8/12">
 			<div class="font-bold">Canada | English (UK) | $(CAD)</div>
-			<div class="grid grid-cols-6">
-				<span>@ 2023 Etsy, Inc.</span>
-				<a href="#" class="underline text-center">Terms of Use</a>
-				<a href="#" class="underline text-center">Privacy</a>
-				<a href="#" class="underline text-center">Interest-based-ads</a>
-				<a href="#" class="underline text-center">Local Shops</a>
-				<a href="#" class="underline text-center">Regions</a>
+			<div class="flex justify-evenly">
+				<span class="mr-5">@ 2023 Etsy, Inc.</span>
+				<a href="#" class="underline">Terms of Use</a>
+				<a href="#" class="underline mr-5">Privacy</a>
+				<a href="#" class="underline mr-5">Interest-based-ads</a>
+				<a href="#" class="underline mr-5">Local Shops</a>
+				<a href="#" class="underline">Regions</a>
 			</div>
 		</div>
 	</div>
