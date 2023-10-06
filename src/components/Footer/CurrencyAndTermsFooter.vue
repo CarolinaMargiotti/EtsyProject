0bea8b3ae89399fb6b9b407a5c4702207ac06181
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-10-06 14:38:29 -0300

    created variables for currencies

--- src/components/Footer/CurrencyAndTermsFooter.vue
+++ src/components/Footer/CurrencyAndTermsFooter.vue
@@ -2,7 +2,9 @@
 	<div class="flex justify-center bg-bluePurple py-5 text-white">
 		<div class="flex justify-between w-8/12">
 			<IconButton class="z-20">
-				<div class="font-bold">Canada | English (UK) | $(CAD)</div>
+				<div class="font-bold">
+					{{ country }} | {{ language }} | $({{ currency }})
+				</div>
 			</IconButton>
 			<div class="flex justify-evenly">
 				<span class="mr-5">@ 2023 Etsy, Inc.</span>
@@ -20,5 +22,9 @@ import { Vue, Component } from "vue-property-decorator";
 import IconButton from "../IconButton.vue";
 
 @Component({ components: { IconButton } })
-export default class CurrencyAndTermsFooter extends Vue {}
+export default class CurrencyAndTermsFooter extends Vue {
+	public country: string = "Canada";
+	public language: string = "English (UK)";
+	public currency: string = "CAD";
+}
 </script>
