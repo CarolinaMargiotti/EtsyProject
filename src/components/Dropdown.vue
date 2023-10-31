b0cfd159f7b48767fc7400f62fb3952fddd29912
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-10-31 13:17:50 -0300

    coded basic functioning dropdown

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,9 +1,27 @@
 <template>
 	<div class="w-full">
-		<IconButton class="z-10 w-full" :fitFormingHover="true">
-			<slot name="title"></slot>
-		</IconButton>
-		<slot name="content"></slot>
+		<div @click="changeContentShow">
+			<IconButton class="z-10 w-full px-4 py-2" :fitFormingHover="true">
+				<div
+					class="flex justify-between w-full text-lg font-bold items-center"
+				>
+					<slot name="title"></slot>
+					<div>
+						<div v-show="!showContent">
+							<i class="fas fa-chevron-down"></i>
+						</div>
+						<div v-show="showContent">
+							<i class="fas fa-chevron-up"></i>
+						</div>
+					</div>
+				</div>
+			</IconButton>
+		</div>
+		<div class="overflow-hidden transition-all">
+			<div v-if="showContent" class="px-5 mt-2 appearContent">
+				<slot name="content"></slot>
+			</div>
+		</div>
 	</div>
 </template>
 <script lang="ts">
@@ -11,5 +29,28 @@ import { Vue, Component } from "vue-property-decorator";
 import IconButton from "./IconButton.vue";
 
 @Component({ components: { IconButton } })
-export default class Dropdown extends Vue {}
+export default class Dropdown extends Vue {
+	public showContent: boolean = false;
+
+	public changeContentShow(): void {
+		this.showContent = !this.showContent;
+	}
+}
 </script>
+<style>
+.appearContent {
+	animation: dropdownContent 0.3s ease-in-out;
+}
+
+@keyframes dropdownContent {
+	0% {
+		opacity: 0;
+		transform: translateY(-100%);
+	}
+
+	100% {
+		opacity: 1;
+		transform: translateY(0%);
+	}
+}
+</style>
--- src/components/Footer/LinksFooter.vue
+++ src/components/Footer/LinksFooter.vue
@@ -1,8 +1,10 @@
 <template>
 	<div class="flex justify-center bg-grayBlue">
-		<div class="text-white grid linksGrid gap-16 pt-10 pb-7 px-7 lg:w-9/12">
+		<div
+			class="text-white grid linksGrid gap-16 pt-10 pb-7 px-7 lg:w-9/12 w-full"
+		>
 			<div v-for="(footer, index) in footers" :key="index">
-				<ul>
+				<ul class="hidden sm:inline">
 					<li class="mb-5">
 						<h5 class="font-bold text-base">{{ footer.title }}</h5>
 					</li>
@@ -20,6 +22,21 @@
 						</div>
 					</div>
 				</ul>
+				<Dropdown class="sm:hidden">
+					<template v-slot:title>{{ footer.title }}</template>
+					<template v-slot:content>
+						<ul class="grid grid-cols-1 gap-7">
+							<li
+								v-for="(subfooter, index) in footer.links"
+								:key="index"
+							>
+								<a :href="subfooter.url">
+									{{ subfooter.title }}
+								</a>
+							</li>
+						</ul>
+					</template>
+				</Dropdown>
 			</div>
 		</div>
 	</div>
@@ -27,12 +44,14 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import OutlinedButton from "../OutlinedButton.vue";
-import IconButton from "../IconButton.vue";
 import { FooterLinks } from "@/models/FooterLinks";
 import { IFooterLinks } from "@/models/IFooterLinks";
 import FooterButtonsGalore from "./FooterButtonsGalore.vue";
+import Dropdown from "../Dropdown.vue";
 
-@Component({ components: { OutlinedButton, IconButton, FooterButtonsGalore } })
+@Component({
+	components: { OutlinedButton, FooterButtonsGalore, Dropdown },
+})
 export default class LinksFooter extends Vue {
 	public footers: IFooterLinks[] = FooterLinks;
 }
@@ -40,5 +59,11 @@ export default class LinksFooter extends Vue {
 <style>
 .linksGrid {
 	grid-template-columns: repeat(3, minmax(0, 1fr)) 1.5fr;
+}
+
+@media (max-width: 639px) {
+	.linksGrid {
+		grid-template-columns: 1fr;
+	}
 }
 </style>
